3171c34755a3fd6de21544a85762be8ff4bad08f
e <6410762+e-mit@users.noreply.github.com>
2023-08-12 16:11:50 +0100

    function to read basic information from git repo.
    Currently tested on permanent test repo

--- gitscan/scanner/scanner.py
+++ gitscan/scanner/scanner.py
@@ -1,5 +1,7 @@
 """Extract information from git repos."""
 from pathlib import Path
+from typing import Any
+from git import Repo  # type: ignore
 
 
 def extract_repo_name(path_to_git: str | Path) -> tuple[str, Path]:
@@ -15,3 +17,56 @@ def extract_repo_name(path_to_git: str | Path) -> tuple[str, Path]:
         repo_name = gitpath.stem
         repo_path = gitpath.parent
     return (repo_name, repo_path)
+
+
+def read_repo(path_to_git: str | Path) -> dict[str, Any]:
+    """Extract basic information about the repo.
+
+    path_to_git is the path to the .git or X.git directory.
+    """
+    repo = Repo(path_to_git)
+    info: dict[str, Any] = {}
+    (info['name'], info['path']) = extract_repo_name(path_to_git)
+    info['bare'] = repo.bare
+    info['remote_count'] = len(repo.remotes)
+    info['branch_count'] = len(repo.branches)  # type: ignore
+    info['tag_count'] = len(repo.tags)
+    if not repo.bare:
+        info['untracked_count'] = len(repo.untracked_files)
+        info['index_changes'] = repo.is_dirty(index=True,
+                                              working_tree=False,
+                                              untracked_files=False,
+                                              submodules=False)
+        info['working_tree_changes'] = repo.is_dirty(index=False,
+                                                     working_tree=True,
+                                                     untracked_files=False,
+                                                     submodules=False)
+        info['stash'] = len(repo.git.stash("list")) > 0
+
+    try:
+        info['branch_name'] = repo.active_branch.name
+        info['detached_head'] = False
+    except TypeError:
+        info['detached_head'] = True
+        info['branch_name'] = "detached"
+
+    info['ahead_count'] = 0
+    info['behind_count'] = 0
+    failed_count = 0
+    for remote in repo.remotes:
+        try:
+            remote.fetch()
+        except Exception:
+            failed_count += 1
+        else:
+            info['ahead_count'] += sum(1 for _ in
+                repo.iter_commits(
+                  f"{remote.name}/{info['branch_name']}..{info['branch_name']}"
+                ))
+            info['behind_count'] += sum(1 for _ in
+                repo.iter_commits(
+                  f"{info['branch_name']}..{remote.name}/{info['branch_name']}"
+                ))
+    info['up_to_date'] = ((info['behind_count'] == 0) and
+                          (info['ahead_count'] == 0))
+    return info
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,6 @@
 import unittest
 from pathlib import Path
+from typing import Any
 from gitscan.scanner import scanner
 
 
@@ -10,20 +11,56 @@ class TestExtractRepoName(unittest.TestCase):
     actual_repo_path = [Path("data"), Path("/fake/folder/data"),
                         Path("data/folder_1")]
 
-    def test_with_Path(self):
+    def test_with_Path(self) -> None:
         for i in range(len(self.git_dir)):
-            self.subTest(i=i)
-            (repo_name, repo_path) = scanner.extract_repo_name(
-                Path(self.git_dir[i]))
-            self.assertEqual(repo_name, self.actual_repo_name[i])
-            self.assertEqual(repo_path, self.actual_repo_path[i])
+            with self.subTest(i=i):
+                (repo_name, repo_path) = scanner.extract_repo_name(
+                    Path(self.git_dir[i]))
+                self.assertEqual(repo_name, self.actual_repo_name[i])
+                self.assertEqual(repo_path, self.actual_repo_path[i])
 
-    def test_with_string(self):
+    def test_with_string(self) -> None:
         for i in range(len(self.git_dir)):
-            self.subTest(i=i)
-            (repo_name, repo_path) = scanner.extract_repo_name(self.git_dir[i])
-            self.assertEqual(repo_name, self.actual_repo_name[i])
-            self.assertEqual(repo_path, self.actual_repo_path[i])
+            with self.subTest(i=i):
+                (repo_name, repo_path) = scanner.extract_repo_name(
+                    self.git_dir[i])
+                self.assertEqual(repo_name, self.actual_repo_name[i])
+                self.assertEqual(repo_path, self.actual_repo_path[i])
+
+
+class TestReadRepo(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.path_to_git = "/home/e/Desktop/data/clean_repo/.git"
+        # create the git repo
+        # do the steps
+        self.expected_info = {'name': 'clean_repo',
+                              'path': Path('/home/e/Desktop/data'),
+                              'bare': False,
+                              'remote_count': 0,
+                              'branch_count': 2,
+                              'tag_count': 1,
+                              'untracked_count': 0,
+                              'index_changes': False,
+                              'working_tree_changes': False,
+                              'stash': True,
+                              'branch_name': 'master',
+                              'detached_head': False,
+                              'ahead_count': 0,
+                              'behind_count': 0,
+                              'up_to_date': True
+                              }
+
+    def tearDown(self) -> None:
+        # delete the git repo
+        ...
+
+    def test_with_Path(self) -> None:
+        info: dict[str, Any] = scanner.read_repo(self.path_to_git)
+        self.assertEqual(set(info.keys()), set(self.expected_info.keys()))
+        for k in self.expected_info:
+            with self.subTest(key=k):
+                self.assertEqual(info[k], self.expected_info[k])
 
 
 if __name__ == '__main__':
