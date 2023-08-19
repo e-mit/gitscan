50b202b45bfa33a7e35750c0a4ce8a2327d6f070
e <6410762+e-mit@users.noreply.github.com>
2023-08-19 15:13:58 +0100

    Shade row colour by repo status.

--- gitscan/gitscan.py
+++ gitscan/gitscan.py
@@ -1,7 +1,8 @@
 import sys
+from typing import Any
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtCore import Qt, QStringListModel, QAbstractListModel, QModelIndex, pyqtSlot
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFont, QColor
 
 from gui.test1 import Ui_MainWindow
 from scanner import search, read
@@ -12,17 +13,32 @@ class MyModel(QAbstractListModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.repo_list = []
+        self.repo_data_list = []
 
-    def data(self, index, role) -> (str | None):
+    def data(self, index: QModelIndex, role: Qt.ItemDataRole) -> Any:
         if role == Qt.ItemDataRole.DisplayRole:
             return str(self.repo_list[index.row()])
         elif role == Qt.ItemDataRole.ToolTipRole:
             return str(self.repo_list[index.row()])
+        elif role == Qt.ItemDataRole.BackgroundRole:
+            return self.row_shading(index)
 
     def rowCount(self, index) -> int:
         return len(self.repo_list)
 
-    def get_commit_html(self, index) -> str:
+    def row_shading(self, index: QModelIndex) -> QColor:
+        if ((self.repo_data_list[index.row()]['untracked_count'] > 0)
+                or self.repo_data_list[index.row()]['working_tree_changes']
+                or self.repo_data_list[index.row()]['index_changes']):
+            return QColor("red")
+        elif (self.repo_data_list[index.row()]['behind_count'] > 0):
+            return QColor("yellow")
+        elif (self.repo_data_list[index.row()]['ahead_count'] > 0):
+            return QColor("green")
+        else:
+            return QColor("white")
+
+    def get_commit_html(self, index: QModelIndex) -> str:
         commit_data = read.read_commits(self.repo_list[index.row()],
                                         VIEW_COMMIT_COUNT)
         summary = ""
@@ -41,6 +57,8 @@ class MyModel(QAbstractListModel):
 
     def get_data(self) -> None:
         self.repo_list = search.find_git_repos("/tmp")
+        for repo in self.repo_list:
+            self.repo_data_list.append(read.read_repo(repo))
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -56,7 +74,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                             self.selection_changed)
         self.model.get_data()
 
-    def set_default_commit_text_format(self):
+    def set_default_commit_text_format(self) -> None:
         font = QFont()
         font.setStyleHint(QFont.StyleHint.Monospace)
         font.setFamily('monospace')
