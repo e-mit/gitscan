a39dead8feaf2a1e59eee0d4829380489a582338
e <6410762+e-mit@users.noreply.github.com>
2023-09-07 17:04:17 +0100

    Exclude git submodules from repo search

--- gitscan/scanner/search.py
+++ gitscan/scanner/search.py
@@ -9,13 +9,17 @@ def find_git_repos(start_dir: str | Path,
                    stop_event: Event | None = None) -> list[str]:
     """Search for repos in all directories inside start_dir.
 
-    Can be stopped early with a threading event.
+    Can be stopped early with a threading event. Full paths listed 
+    in exclude_dirs (and all their descendants) are excluded.
+    Git submodule directories are also excluded.
     """
     list_path_to_git = []
     for root, dirs, files in os.walk(start_dir):
         if (stop_event is not None) and stop_event.is_set():
             break
-        dirs[:] = [x for x in dirs if (Path(root)/x) not in exclude_dirs]
+        root_path = Path(root)
+        dirs[:] = [x for x in dirs if (root_path/x) not in exclude_dirs
+                   and not ((x == "modules") and (root_path.stem == ".git"))]
         if "HEAD" in files and "refs" in dirs and "objects" in dirs:
             list_path_to_git.append(root)
     list_path_to_git.sort()
