ec85b2741076879a4b0f03c52b1d5e32b1bc1fff
e <6410762+e-mit@users.noreply.github.com>
2023-09-11 17:29:08 +0100

    Prevent __main__ from showing as app name in argparse help

--- __main__.py
+++ __main__.py
@@ -6,15 +6,16 @@ from .gitscan import gitscan
 
 DEFAULT_LOG_LEVEL = 'ERROR'
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    help_message = ("Choose stdout logging level."
-                    f" Default: {DEFAULT_LOG_LEVEL}."
-                    " Valid values: "
-                    + ", ".join(logging._nameToLevel.keys()))
-    parser.add_argument('-log',
-                        '--loglevel',
-                        default=DEFAULT_LOG_LEVEL,
-                        help=help_message)
-    log_level = parser.parse_args().loglevel.upper()
-    gitscan.main(log_level)
+__name__ = "hello"
+
+parser = argparse.ArgumentParser(prog="python -m " + gitscan.APP_TITLE.lower())
+help_message = ("Choose stdout logging level."
+                f" Default: {DEFAULT_LOG_LEVEL}."
+                " Valid values: "
+                + ", ".join(logging._nameToLevel.keys()))
+parser.add_argument('-log',
+                    '--loglevel',
+                    default=DEFAULT_LOG_LEVEL,
+                    help=help_message)
+log_level = parser.parse_args().loglevel.upper()
+gitscan.main(log_level)
